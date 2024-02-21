b95fa596ab984d73a5d4293973c05ca1b24ace8c
KPGDevs <KPGDevs@gmail.com>
2024-02-21 23:05:22 +0100

    Not used imports deleted

--- src/main/java/com/loginthyme/security/controller/AdminPageController.java
+++ src/main/java/com/loginthyme/security/controller/AdminPageController.java
@@ -3,14 +3,11 @@ package com.loginthyme.security.controller;
 import com.loginthyme.security.model.MyUser;
 import com.loginthyme.security.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
--- src/main/java/com/loginthyme/security/controller/RegisterController.java
+++ src/main/java/com/loginthyme/security/controller/RegisterController.java
@@ -11,12 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 @Controller
